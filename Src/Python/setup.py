5ceb26a25cecf9cd04b0f08bc97ecc1ff7b704db
Muhammad Ali Bhutto <mb01468@st.habib.edu.pk>
2019-11-27 15:20:48 +0500

    Updates

---
+++ Src/Python/setup.py
@@ -0,0 +1,16 @@
+import cx_Freeze
+from cx_Freeze import *
+
+setup(
+    name = "GeneTech",
+    options = {'build_exe':{'packages':['pyqt5', 'SchemDraw',
+                                        'matplotlib', 'dnaplotlib',
+                                        'py4j', 'sbol', 'PIL',]}},
+    executables = [
+        Executable(
+            "Genetech.py",
+            )
+        ] 
+
+
+    )
