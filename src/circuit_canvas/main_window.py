e79f725e4dbd963ba48165f56dd11753ef28c79d
Mudasir Hanif <ms03831@st.habib.edu.pk>
2020-07-29 17:35:01 +0500

    refactored save method

--- src/circuit_canvas/main_window.py
+++ src/circuit_canvas/main_window.py
@@ -8,12 +8,25 @@ from main_widget import MainScreenWidget
 class MainCircuitWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        
+        self.file_name_image = None
         self.initUI()
+		
     
     def initUI(self):
+	
+		self.menu = self.menuBar()
+		self.file_menu = self.menu.addMenu("File")
+		capture_action = QAction("Save Image", self)
+		capture_action.setShortcut("Ctrl+S")
+		capture_action.setToolTip("Capture image of the circuit you have drawn")
+		capture_action.triggered.connect(self.captureCircuit)
+		
+		self.file_menu.addAction(capture_action)
+		
+		
         self.setGeometry(100, 100, 800, 600)
         self.setWindowTitle("GeneTech - Circuit Builder")
+		
         
         self.setWindowIcon(QIcon('../SmallLogo.png'))
         
@@ -24,8 +37,13 @@ class MainCircuitWindow(QMainWindow):
         
     
     def captureCircuit(self):
-        self.main_widget.scene.grScene.save("circuit.jpg")
-
+		if not self.file_name_image:
+			filename, filter = QFileDialog.getSaveFileName(self, "Save Circuit To Image")
+			self.file_name_image = filename
+			self.main_widget.scene.grScene.save(filename)
+		else:
+			self.main_widget.scene.grScene.save(self.file_name_image)
+		
     
         
 
